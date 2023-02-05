19d3c204d33d4d6c916872cfd4cb60d6c6bfd72d
awholehorse <jason_m02@aol.com>
2023-02-04 22:29:51 -0500

    Roots stop things now

---
+++ Assets/Scripts/RootControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootControl : MonoBehaviour
+{
+    public RobotControler Robot;
+    public float robotSpeed = 2f;
+    Coroutine wait = null;
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(wait == null)
+        {
+            wait = StartCoroutine(Wait());
+        }
+        wait = null;
+    }
+
+    public IEnumerator Wait()
+    {
+        Robot.moveSpeed = 0f;
+        yield return new WaitForSecondsRealtime(5);
+        Robot.moveSpeed = robotSpeed;
+    }
+}
