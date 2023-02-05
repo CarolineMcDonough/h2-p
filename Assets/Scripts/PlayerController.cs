2d52e1c46ab8e1c5e9c2c37c7dc809d0645464ac
awholehorse <jason_m02@aol.com>
2023-02-05 01:16:16 -0500

    Added Fertilizer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform movePoint;
+    public Transform robotMove;
+    Transform cur;
 
     public LayerMask whatStopsMovement;
-    public LayerMask pickupLayer;
 
     public Animator anim;
+
+    public int seed;
+    public GameObject root;
 
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
+        Physics2D.IgnoreCollision(robotMove.GetComponent<Collider2D>(), movePoint.GetComponent<Collider2D>());
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        
+
+        if(seed > 0 && Input.GetKeyDown("l"))
+        {
+            cur = movePoint;
+            Instantiate(root, cur);
+            seed -= 1;
+        }
 
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
